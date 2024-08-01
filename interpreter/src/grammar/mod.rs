bbeebc7f5c410d8c285f543d5803281bf707dd63
xxxAnn <ann.mauduy.decius@gmail.com>
2024-07-31 23:08:45 -0400

    detail more grammar

--- interpreter/src/grammar/mod.rs
+++ interpreter/src/grammar/mod.rs
@@ -3,10 +3,16 @@
 /// 
 /// Building Blocks:
 /// Ignore EOS: [<EOS>]*
+/// Name List: [<Name> <Comma> <{Ignore EOS}>]*
 /// Simple List: [<Name/Literal> <Comma> <{Ignore EOS}>]*
 /// Assign List: [<Name> <Equal> <Name/Literal> <Comma> <{Ignore EOS}>]*
 /// Function Call: <Name> <LeftParen> <{Simple List}> <RightParen>
 /// Table Body: <LeftCurly> <{Assign List}> <RightCurly>
 /// 
+/// Special:
+/// Compilables: [<{Compilable}>]*
+/// 
 /// Compilable:
+/// Root: <{Compilables}>
 /// Assignment: <Name> <Equal> <Name/Literal/{Function Call}/{Table Body}>
+/// Function Definition: <Function> <Name> <LeftParen> <{Name List}> <RightParen> <{Compilables}> <End>
